2f0ff43f20e2ebe51895d22e651a1d7207a1bac5
MatL52 <119827318+MatL52@users.noreply.github.com>
2023-02-15 16:16:16 +0900

    こみっとする「

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello CodeSandbox</h1>
+//       <h2>Start editing to see some magic happen!</h2>
+//     </div>
+//   );
+// }
+
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>こんにちは</h1>
+      <p>お元気ですか</p>
+    </>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -12,13 +12,37 @@
 //   </StrictMode>
 // );
 
-import { StrictMode } from "react";
-import ReactDom from "react-dom";
-import App from "./App";
+// import { StrictMode } from "react";
+// import ReactDom from "react-dom";
+// import App from "./App";
 
-ReactDom.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById("root")
-);
+// ReactDom.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+//   document.getElementById("root")
+// );
+
+//reactを使うときの準備として、Riactを読み込む処理が最初に必要
+import React from "react";
+import ReactDom from "react-dom";
+
+// Reactでは関数の中でHTMLタグを書く
+// 出力する内容はreturnの先でHTMLとほぼ同じ構造で記述する
+// 複数のHTMLタグを表示する場合は（）を使って記述する
+// divタグで囲むとHTMLの入れ子構造が変わってしまうので
+// <React.Fragment>もしくは<>で囲む
+
+const App = () => {
+  return (
+    // <React.Fragment>
+    <>
+      <h1>こんにちは</h1>
+      <p>お元気ですか</p>
+      {/* </React.Fragment> */}
+    </>
+  );
+};
+
+//Render関数
+ReactDom.render(<App />, document.getElementById("root"));
